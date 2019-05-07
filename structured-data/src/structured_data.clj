0f78fb0581cbe09c8852e30ef665a5a9253419d7
Ed Maphis <emaphis85@gmail.com>
2019-05-06 23:09:49 -0400

    Okey doke.

--- structured-data/src/structured_data.clj
+++ structured-data/src/structured_data.clj
@@ -3,7 +3,7 @@
 ;;(use 'clojure.repl)
 
 
-;;; `let` expression - local names
+;;; `Let` expression - local names
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (let [x 42]
@@ -132,7 +132,6 @@
 (sum-pairs [64 256] [-51 -219]) ;; => [13 37]
 
 
-
 ;;; Boxes
 ;;;;;;;;;;
 
@@ -149,8 +148,6 @@
 ;; Write the functions (height rectangle) and (width rectangle) that
 ;; return the height and width of the given rectangle.
 ;; Use destructuring.
-
-
 
 (defn height [rectangle]
   (let [[[_ y1] [_ y2]] rectangle]
@@ -192,42 +189,147 @@
 ;; given rectangle.
 
 (defn area [rectangle]
-  :-)
+  (* (width rectangle)
+     (height rectangle)))
+
+(area (rectangle [1 1] [5 1])) ;; => 0
+(area (rectangle [0 0] [1 1])) ;; => 1
+(area (rectangle [0 0] [4 3])) ;; => 12
+(area (rectangle [3 1] [10 4]));; => 21
 
 
+;;  Exercise 8
+
+;; Write the function `(contains-point? rectangle point)` that returns `true` if
+;; `rectangle` contains `point` and otherwise `false`.
+
+;; Remember that you can give `<=` multiple parameters. `(<= x y z`) returns `true`
+;; if  /() x \leq y \leq z\) holds. Otherwise `false`.
+
+;; Hint: `and` is useful.
 
 (defn contains-point? [rectangle point]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle
+        [x y] point]
+    (and (<= x1 x x2)
+         (<= y1 y y2))))
+
+(contains-point? (rectangle [0 0] [2 2])
+                 (point 1 1))            ;=> true
+(contains-point? (rectangle [0 0] [2 2])
+                 (point 2 1))            ;=> true
+(contains-point? (rectangle [0 0] [2 2])
+                 (point -3 1))           ;=> false
+(contains-point? (rectangle [0 0] [2 2])
+                 (point 1 3))            ;=> false
+(contains-point? (rectangle [1 1] [2 2])
+                 (point 1 1))            ;=> true
+(contains-point? (rectangle [1 1] [1 1])
+                 (point 1 1))            ;=> true
 
 
+;;  Exercise 9
+
+;; Write the function (contains-rectangle? outer inner) that returns `true` if the
+;; `rectangle` inner is inside the rectangle outer and otherwise `false`.
+
+;; Hint: use contains-point?
 
 (defn contains-rectangle? [outer inner]
-  :-)
+  (let [[pt1 pt2] inner]
+    (and (contains-point? outer pt1)
+         (contains-point? outer pt2))))
+
+(contains-rectangle? (rectangle [0 0] [3 3])
+                     (rectangle [1 1] [2 2])) ;=> true
+(contains-rectangle? (rectangle [0 0] [2 2])
+                     (rectangle [1 1] [3 3])) ;=> false
+(contains-rectangle? (rectangle [0 0] [1 1])
+                     (rectangle [0 0] [1 1])) ;=> true
+(contains-rectangle? (rectangle [0 0] [1 1])
+                     (rectangle [1 1] [2 2])) ;=> false
 
 
+;;; Maps
+;;;;;;;;;;;
+
+{"foo" 42, "bar" 666}
+{"mehmeh" (+ 2 5)
+ "rupatipor" "ropopo"}
+
+;; indexed with get function.
+(let [ages {"Juhana" 3
+            "Ilmari" 42
+            "King of All Cosmos" -6}]
+  (get ages "King of All Cosmos"))
+;; => -6
+(comment
+  (def book {:title "The City and the City"
+             :authors [{:name "China Mieville", :birth-year 1972}]})
+
+  (get book :title) ;; => "The City and the City"
+
+  ;; keys act as lookup functions
+  (:title book) ;; => "The City and the City"
+  (def x 1)
+  )
+
+;; functions on collections
+(count [ 1 2 3]) ;; => 3
+(count {:name "China Miéville", :birth-year 1972}) ;; => 2
+(count ":)") ;; => 2
+
+
+;; authors and books
+
+
+;;  Exercise 10
+
+;; Write the function (title-length book) that counts the length of the book’s title.
 
 (defn title-length [book]
-  :-)
+  (count (:title book)))
 
 
+;; Exercise 11
+
+;; Write the function (author-count book) that returns the amount of authors that book
+;; has.
 
 (defn author-count [book]
-  :-)
+  (count (:authors book)))
 
 
+;;  Exercise 12
+
+;; Write the function (multiple-authors? book) that returns `true` if book has multiple
+;; authors, otherwise `false`
 
 (defn multiple-authors? [book]
-  :-)
+  (> (author-count book) 1))
 
+
+;; Adding values to a Map or Vector
+
+(assoc {:a 1} :b 2) ;; => {:a 1, :b 2}
+(assoc {:a 1} :a 2) ;; => {:a 2}
+(assoc [3 2 1] 1 "~o~") ;; => [3 "~o~" 1]
+
+(let [original [1 2 3 4]
+      new      (assoc original 2 "foo")]
+  original)
+;; => [1 2 3 4]
 
 
 (defn add-author [book new-author]
-  :-)
+  (let [authors (:authors book)]
+    (assoc book :authors (conj authors new-author))))
 
 
 
 (defn alive? [author]
   :-)
+
 
 
 
